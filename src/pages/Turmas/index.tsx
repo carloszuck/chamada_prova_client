bbccc0b43c4dd7def093e363a89e290cd5d31584
carlos_zuck <carlosmiguelzpb.cmzpb77@gmail.com>
2021-06-17 23:35:17 -0300

    delete student

--- src/pages/Turmas/index.tsx
+++ src/pages/Turmas/index.tsx
@@ -1,32 +1,82 @@
-import { Form, Button } from 'react-bootstrap';
+import React, { useEffect, useState } from "react";
+import { Button, Col, Container, Row, Table } from "react-bootstrap";
+import { faPencilAlt, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import api from '../../services/api';
+import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
-const Turmas: React.FC = () => {
-    return (
-      <>
-        <h2 className="p-4">Página Exemplo</h2>
+export interface StudentInterface {
+  _id: string;
+  name: string;
+  studentClass: string;
+  num: number;
+  ra: number;
+};
 
-        <Form>
-          <Form.Group controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-            <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
-            </Form.Text>
-          </Form.Group>
+const StudentList: React.FC = () => {
+  const [students, setStudent] = useState<StudentInterface[]>([]);
+  const history = useHistory();
 
-          <Form.Group controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
-          </Form.Group>
-          <Form.Group controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Check me out" />
-          </Form.Group>
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>
-        </Form>
-      </>
-    )
+  useEffect(() => {
+    async function getStudent() {
+      const students = await api.get('/students');
+
+      setStudent(students.data);
+    }
+
+    getStudent();
+  }, [])
+
+  const [deletedStudentId, setDeletedStudentId] = useState('0');
+  async function delStudent() {
+    const deletedStudent = await api.delete(`/del_students/${deletedStudentId}`)
+
+    window.location.reload();
   }
-  
-  export default Turmas;
+
+  return (
+    <Container>
+      <Row className='mt-4'>
+        <Col><h2>Listagem de Alunos</h2></Col>
+      </Row>
+      <Row className='mt-2'>
+        <Col>
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>Nome</th>
+                <th>Classe</th>
+                <th>N°</th>
+                <th>RA</th>
+                <th>Editar</th>
+                <th>Remover</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                students.map(student => {
+                  return (
+                    <tr>
+                      <td>{student.name}</td>
+                      <td>{student.studentClass}</td>
+                      <td>{student.num}</td>
+                      <td>{student.ra}</td>
+                      <th><Link to={`/students/${student._id}`}><FontAwesomeIcon icon={faPencilAlt} /></Link></th>
+                      <th><FontAwesomeIcon icon={faTrash} onClick={() => {
+                        setDeletedStudentId(`${student._id}`); 
+                        delStudent();
+                      }} /></th>
+                    </tr>
+                  )
+                })
+              }
+            </tbody>
+          </Table>
+        </Col>
+      </Row>
+    </Container>
+  )
+}
+
+export default StudentList;
